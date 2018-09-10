1ed52f34ea25eb4d7c6f2f1923bbd7c88c4181ec
meladmagdi <meladmagdi9@gmail.com>
2018-09-10 12:14:20 +0200

    this is we change in 9/9/2018

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,66 @@
-var gulp = require ('gulp'),
-    //sass = require('gulp-sass'),
-    uglify = require ('gulp-uglify'),
-    plumber = require ('gulp-plumber');
+var gulp        = require ('gulp'),
+    uglify      = require('gulp-uglify'),
+    header      = require('gulp-header'),
+    rename      = require('gulp-rename'),
+    plumber     = require('gulp-plumber'),
+    imagemin    = require('gulp-imagemin'),
+    pkg         = require('./package.json'),
+    cleanCSS    = require('gulp-clean-css'),
+    autoprefix  = require('autoprefixer'),
+    rucksack    = require('rucksack-css'),
+    postcss     = require('gulp-postcss'),
+    sass        = require('gulp-ruby-sass'),
+    lost        = require('lost');
+
+var files = {
+  title: 'project',
+  version: '1.0',
+  desginer: {
+    name: 'melade & mena',
+    url: 'www.jesus.com'
+  },
+  inSass: 'vindor/style.scss',
+  outCss: 'puplic/css/',
+  css: 'puplic/css/',
+  js: 'puplic/js/*.js'
+};
+
+// Copyright
+var banner = ['/*!\n',
+  ' * '+ files.title +' v'+ files.version +' Copyright &copy; '+ (new Date()).getFullYear() +'\n',
+  ' * All Desgin &amp; code by '+ files.desginer.name +' | '+ files.desginer.url +'\n',
+  ' */\n', ''].join('');
 
 // uglify For javaScript mini
 gulp.task('scripts', function(){
-  gulp.src('puplic/js/*.js').
-  pipe(plumber()).
-  pipe(uglify()).
-  pipe(gulp.dest('minjs'));
+  gulp.src(files.js)
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('minjs'));
 });
 //sass for compile the code
-/*gulp.task('style', function(){
-  gulp.src('vindor/*.scss').
-  pipe(sass({style: 'compressed'})).
-  pipe(gulp.dest('puplic/css'));
-});*/
+gulp.task('sass', function(){
+  var processors = [
+    lost(),
+    rucksack(),
+    autoprefix()
+  ];
+
+  gulp.src(files.inSass)
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss(processors))// use processors
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest(files.outCss))// convert to css
+      .pipe(cleanCSS())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(files.outCss));// css to minify
+});
 
 // gulp watch is fot watching file
 
 gulp.task('watch', function(){
-  gulp.watch('puplic/js/*.js', ['scripts']);
+  gulp.watch(files.js, ['scripts']);
 });
 
-gulp.task('default', ['scripts','watch']);
+gulp.task('default', ['scripts','sass','watch']);
